6738204a3e5024449836e9f95c9c28379ec118d9
David Šajina <57251489+David-Sajina@users.noreply.github.com>
2022-11-04 17:57:08 +0100

    Fix  click

--- src/CardFlip.js
+++ src/CardFlip.js
@@ -17,26 +17,29 @@ class CardFlip extends React.Component {
 
 	render() {
 		return (
-			<ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+			<ReactCardFlip
+				isFlipped={this.state.isFlipped}
+				flipDirection="horizontal"
+			>
 				<div className="front">
 					<div className="image">
 						<img
 							alt="dog"
 							style={{ width: 500, height: 500 }}
+							onClick={this.handleClick}
 							src="https://www.svgrepo.com/show/327388/logo-react.svg"
 						/>
 					</div>
-					<button onClick={this.handleClick}>Click to flip</button>
 				</div>
 				<div className="back">
 					<div className="noimage">
 						<img
 							style={{ width: 500, height: 500 }}
 							alt="dog"
+							onClick={this.handleClick}
 							src="https://mui.com/static/logo.png"
 						/>
 					</div>
-					<button onClick={this.handleClick}>Click to flip</button>
 				</div>
 			</ReactCardFlip>
 		);
