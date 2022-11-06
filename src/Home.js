b2ce0ed02d0fc3f84944495793ebb47fd7655ee8
Andrej Korica <koricaandrej3@gmail.com>
2022-11-06 16:20:02 +0100

    image sorting

--- src/Home.js
+++ src/Home.js
@@ -5,16 +5,19 @@ import CardFlip from "./components/CardFlip";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import Grid from '@mui/material/Grid';
-
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import "./index.css";
 
 const Home = () => {
   const [podaci, setPodaci] = useState([]);
+  const [poredak, setPoredak] = useState(4);
 
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
+  
   const componentDidMount = async () => {
     const podaci = await axios.get(`http://localhost:8000/photos`);
     setPodaci(podaci.data);
@@ -36,27 +39,56 @@ const Home = () => {
     p: 4,
   };
 
-
   return (
     <div>
+      <Stack direction="row" spacing={2} justifyContent="center">
+        <Button
+          variant="outlined"
+          onClick={() => {
+            setPoredak(4);
+          }}
+          sx={{width: '8rem'}}
+        >
+          instagram
+        </Button>
+        <Button
+          variant="outlined"
+          sx={{width: '8rem'}}
+          onClick={() => {
+            setPoredak(12);
+          }}
+          width="5"
+        >
+          Inline
+        </Button>
+        </Stack>
+
       
-      <Grid container spacing={2} gap="1" justifyContent="center">
-      {podaci.map((person) => (
-        <Grid item key={person.id}>
-        <p>{person.Name}</p>
-        <img
-          onClick={handleOpen}
-          src={person.src}
-          alt="IMG DIDNT LOAD"
-          
-        />
+        <Grid container spacing={1}>
+          {podaci.map((person) => (
+            <Grid key={person.id} lg={poredak} md ={poredak} item >
+              <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                
+                
+              >
+                <p>{person.Name}</p>
+
+                <img
+                  className= "imageGallery"
+                  onClick={handleOpen}
+                  src={person.src}
+                  alt="IMG DIDNT LOAD"
+                  
+                />
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
      
-     </Grid>
-      ))}
-      </Grid>
-    
-    
-      
+
       <Modal
         open={open}
         onClose={handleClose}
