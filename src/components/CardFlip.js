0e6b17b488388538fdef7de841dc03c556e31072
Andrej Korica <koricaandrej3@gmail.com>
2022-12-11 14:36:48 +0100

    alignments

--- src/components/CardFlip.js
+++ src/components/CardFlip.js
@@ -3,55 +3,42 @@ import { useState } from "react";
 import LoopIcon from "@mui/icons-material/Loop";
 
 const CardFlip = ({ podaci }) => {
-	const [isFlipped, setisFlipped] = useState(true);
-	console.log(podaci);
+  const [isFlipped, setisFlipped] = useState(true);
+  console.log(podaci);
 
-	return (
-		<ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-			
-				
-					<div className="positionFlip">
-						<div className="containContainContain">
-							<div className="containNA">
-								<div className="naslovBijeli">title:</div>
-								<div className="containFlip">{podaci.Name}</div>
-							</div>
-							<div className="containNA">
-								<div className="naslovBijeli">Author:</div>
-								<div className="containFlip">{podaci.Author}</div>
-							</div>
-						</div>
-							<div className="naslovBijeli">Description:</div>
-							<div className="containLarge">{podaci.Description}</div>	
-						<button
-							className="buttonFlip2"
-							onClick={() => setisFlipped(!isFlipped)}
-						>
-							<LoopIcon fontSize="large" className="img1size" />
-						</button>
-					</div>
-				
-			
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div className="positionFlip">
+        <div className="containContainContain">
+          <div className="containNA">
+            <div className="naslovBijeli">title:</div>
+            <div className="containFlip">{podaci.Name}</div>
+          </div>
+          <div className="containNA">
+            <div className="naslovBijeli">Author:</div>
+            <div className="containFlip">{podaci.Author}</div>
+          </div>
+        </div>
+        <div className="naslovBijeli">Description:</div>
+        <div className="containLarge">{podaci.Description}</div>
+        <button className="buttonFlip" onClick={() => setisFlipped(!isFlipped)}>
+          <LoopIcon fontSize="large" className="img1size" />
+        </button>
+      </div>
 
-			<div className="back">
-				<div className="noimage">
-				<button
-						className="buttonFlip"
-						onClick={() => setisFlipped(!isFlipped)}
-					>
-						<LoopIcon fontSize="large" className="img1size" />
-					</button>
-					<img
-						className="alignSlikaModal"
-						style={{ height: "600px" }}
-						alt="pic"
-						src={podaci.src}
-					/>
-					
-				</div>
-			</div>
-		</ReactCardFlip>
-	);
+      <div className="back">
+        <button className="buttonFlip" onClick={() => setisFlipped(!isFlipped)}>
+          <LoopIcon fontSize="large" className="img1size" />
+        </button>
+        <img
+          className="alignSlikaModal"
+          style={{ height: "600px" }}
+          alt="pic"
+          src={podaci.src}
+        />
+      </div>
+    </ReactCardFlip>
+  );
 };
 
 export default CardFlip;
